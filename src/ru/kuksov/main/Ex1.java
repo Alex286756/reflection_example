ff58771fe1b72d1911d3dcdc9c197d359a43936b
Alex286756 <ai286756@gmail.com>
2023-09-07 21:56:19 +0600

    get fields

--- src/ru/kuksov/main/Ex1.java
+++ src/ru/kuksov/main/Ex1.java
@@ -1,7 +1,27 @@
 package ru.kuksov.main;
 
-public class Ex1 {
-    public static void main(String[] args) {
+import java.lang.reflect.Field;
 
+public class Ex1 {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException {
+        Class employeeClass = Class.forName("ru.kuksov.main.Employee");
+//        Class employeeClass2 = Employee.class;
+//
+//        Employee emp = new Employee();
+//        Class employeeClass3 = emp.getClass();
+
+        Field someField= employeeClass.getField("id");
+        System.out.println(("Type of id field = " + someField.getType()));
+        System.out.println("*****");
+        Field[] fields = employeeClass.getFields();
+        for (Field field: fields){
+            System.out.println("Type of " + field.getName() + " = " + field.getType());
+        }
+
+        System.out.println("*****");
+        Field[] allFields = employeeClass.getDeclaredFields();
+        for (Field field: allFields){
+            System.out.println("Type of " + field.getName() + " = " + field.getType());
+        }
     }
 }

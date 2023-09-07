80e673ce3ead0ffaa20af59975238c698828d02d
Alex286756 <ai286756@gmail.com>
2023-09-07 22:26:35 +0600

    create object of class

---
+++ src/ru/kuksov/main/Ex2.java
@@ -0,0 +1,27 @@
+package ru.kuksov.main;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class Ex2 {
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Class employeeClass = Class.forName("ru.kuksov.main.Employee");
+
+//        Employee o = (Employee) employeeClass.newInstance();
+//        System.out.println(o);
+
+        Constructor<Employee> constructor1 = employeeClass.getConstructor();
+        Employee obj1 = constructor1.newInstance();
+
+        Constructor constructor2 = employeeClass.getConstructor(int.class, String.class, String.class);
+        Object obj2 = constructor2.newInstance(5, "Bob", "IT");
+        System.out.println(obj2);
+
+        System.out.println("---------------------------");
+
+        Method method = employeeClass.getMethod("setSalary", double.class);
+        method.invoke(obj2, 800.88);
+        System.out.println(obj2);
+    }
+}
